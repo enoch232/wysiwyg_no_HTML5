5d843aee1aa33c6518b9e192299e86e3b0827674
enoch232 <enoch232@gmail.com>
2016-06-04 14:16:27 -0700

    remove caratPosition

--- client-side/wysiwyg.js
+++ client-side/wysiwyg.js
@@ -14,31 +14,6 @@ cross_button.addEventListener("click",function(){
 function applyStyle(style){
 	editor.value = editor.value.substring(0, editor.selectionStart)+ style + getTextFieldSelection(editor)+"</span>"+editor.value.substring(editor.selectionEnd, editor.value.length);
 }
-function doGetCaretPosition (oField) {
-
-  // Initialize
-  var iCaretPos = 0;
-  // IE Support
-  if (document.selection) {
-    // Set focus on the element
-    oField.focus();
-    // To get cursor position, get empty selection range
-    var oSel = document.selection.createRange();
-
-    // Move selection start to 0 position
-    oSel.moveStart('character', -oField.value.length);
-
-    // The caret position is selection length
-    iCaretPos = oSel.text.length;
-  }
-
-  // Firefox support
-  else if (oField.selectionStart || oField.selectionStart == '0')
-    iCaretPos = oField.selectionStart;
-
-  // Return results
-  return iCaretPos;
-}
 function getTextFieldSelection(textField) {
 	console.log(textField.selectionStart);
     return textField.value.substring(textField.selectionStart, textField.selectionEnd);
