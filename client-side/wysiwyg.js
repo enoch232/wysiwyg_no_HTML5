c039935bc0dad559ef1ba7e3250f66463241ecd8
enoch232 <enoch232@gmail.com>
2016-06-04 14:15:43 -0700

    change html file name, add styling, and add more functionality

--- client-side/wysiwyg.js
+++ client-side/wysiwyg.js
@@ -1,14 +1,16 @@
-
-var italic_button = document.getElementById("italic_button");
 var editor = document.getElementById("editor");
 italic_button.addEventListener("click",function(){
-	style = "<span style = 'font-style:italic'>"
+	style = "<span style = 'font-style:italic'>";
 	applyStyle(style);
 });
 underline_button.addEventListener("click",function(){
-	style = "<span style = 'text-decoration:underline'>"
+	style = "<span style = 'text-decoration:underline'>";
 	applyStyle(style);
 });
+cross_button.addEventListener("click",function(){
+	style = "<span style = 'text-decoration:line-through'>";
+	applyStyle(style);
+})
 function applyStyle(style){
 	editor.value = editor.value.substring(0, editor.selectionStart)+ style + getTextFieldSelection(editor)+"</span>"+editor.value.substring(editor.selectionEnd, editor.value.length);
 }
--- server-side/app.js
+++ server-side/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/',function(req, res){
 	res.end(req);
@@ -9,5 +10,5 @@ app.post('/', function(req, res){
 	res.send(req.body.editor);
 	res.end();
 });
-app.listen(3000);
-console.log("Wysiwyg server is running at port ");
+app.listen(port);
+console.log("Wysiwyg server is running at port "+port);
